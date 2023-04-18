26547127b9e6b2c53445abd3b5fe6e379afa7f7a
Viktor Seniukov <spinnekopser@gmail.com>
2023-04-18 20:18:42 +0600

    welcome to hell

--- source/js/script.js
+++ source/js/script.js
@@ -1,20 +1,22 @@
-const menuInput = document.querySelector('.navigation-button__input');
+const menuButton = document.querySelector('.navigation__button');
 const navigationList = document.querySelector('.navigation__list');
 
-const onMenuInputChange = () => {
+const onMenuButtonClick = () => {
+  menuButton.classList.toggle('navigation-button--open');
   navigationList.classList.toggle('navigation__list--open');
-  navigationList.classList.toggle('navigation__list--close');
 }
 
 const loadJavaScript = () => {
+  const nojs = document.querySelector('.nojs');
   const nojsHeader = document.querySelector('.nojs__header');
   const nojsNavigationButton = document.querySelector('.nojs__navigation-button');
-  const nojsNavigationList = document.querySelector('.nojs__navigation__list');
+  const nojsNavigationList = document.querySelector('.nojs__navigation-list');
 
+  nojs.classList.remove('nojs');
   nojsHeader.classList.remove('nojs__header');
   nojsNavigationButton.classList.remove('nojs__navigation-button');
-  nojsNavigationList.classList.remove('nojs__navigation__list');
+  nojsNavigationList.classList.remove('nojs__navigation-list');
 };
 
 loadJavaScript();
-menuInput.addEventListener('change', onMenuInputChange);
+menuButton.addEventListener('click', onMenuButtonClick);
