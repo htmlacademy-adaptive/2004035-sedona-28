d1c29c2a8e2741074171b5fdd5648641e9cbc55a
Viktor Seniukov <spinnekopser@gmail.com>
2023-04-19 23:03:00 +0600

    gulp part 1

--- gulpfile.js
+++ gulpfile.js
@@ -4,37 +4,108 @@ import sass from 'gulp-dart-sass';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
-import postimport from 'postcss-import';
-import posturl from 'postcss-url';
-import postsass from 'postcss-sass';
 import notify from 'gulp-notify';
+import csso from 'postcss-csso';
+import rename from 'gulp-rename';
+import htmlmin from 'gulp-htmlmin';
+import squoosh from 'gulp-libsquoosh';
+import del from 'del';
+import terser from 'gulp-terser';
+import svgmin from 'gulp-svgmin';
+import groupCssMediaQueries from 'gulp-group-css-media-queries';
+import svgSprite from 'gulp-svg-sprite';
 
 // Styles
 
-export const styles = () => {
+const styles = () => {
   return gulp.src('source/sass/style.scss', { sourcemaps: true })
-    // .pipe(plumber({
-    //   errorHandler: notify.onError(function(err) {
-    //     return {
-    //       title: 'Task styles',
-    //       message: "Error: <%= error.message %>",
-    //       sound: true
-    //     }
-    //   })
-    // }))
+    .pipe(plumber({
+      errorHandler: notify.onError(function(err) {
+        return {
+          title: 'SCSS',
+          message: "Error: <%= error.message %>",
+          sound: true
+        }
+      })
+    }))
     .pipe(plumber())
     .pipe(sass().on('error', sass.logError))
-    // .pipe(postcss([
-    //   postimport(),
-    //   posturl()
-    // ], {
-    //   syntax: postsass
-    // }))
+    .pipe(groupCssMediaQueries())
+    // .pipe(gulp.dest('source/css'))
     .pipe(postcss([
-      autoprefixer()
+      autoprefixer(),
+      csso()
     ]))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
+}
+
+// HTML
+
+const html = () => {
+  return gulp.src('source/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('build'))
+}
+
+// Script
+
+const sctipt = () => {
+  return gulp.src('source/js/*js')
+    .pipe(terser())
+    .pipe(gulp.dest('build/js'))
+}
+
+// Image
+
+const optimiseImage = () => {
+  return gulp.src(['source/img/**/*.{jpg,png}', '!source/img/**/*.svg'])
+    .pipe(squoosh())
+    .pipe(gulp.dest('build/img'))
+}
+
+// WebP
+
+const createWebp = () => {
+  return gulp.src('source/img/*.{jpg,png}')
+    .pipe(squoosh({
+      webp: {}
+    }))
+    .pipe(gulp.dest('build/img'))
+}
+
+// SVG
+
+const svg = () => {
+  return gulp.src(['source/img/**/*.svg', '!source/img/svg'])
+    .pipe(svgmin())
+    .pipe(gulp.dest('build/img'))
+}
+
+const sprite = () => {
+  return gulp.src('source/img/svg/*.svg')
+    .pipe(svgSprite({
+      mode: {
+        stack: {
+          sprite: '../sprite.svg'
+        }
+      }
+    }))
+    .pipe(gulp.dest('build/img'))
+}
+
+// Clean
+
+const clean = () => {
+  return del('build')
+}
+
+// Copy
+
+const copy = () => {
+  return gulp.src('source/fonts/*.*', { base: 'source' })
+    .pipe(gulp.dest('build'))
 }
 
 // Server
@@ -58,6 +129,21 @@ const watcher = () => {
   gulp.watch('source/*.html').on('change', browser.reload);
 }
 
+const build = gulp.series(
+  clean,
+  gulp.parallel(
+    html,
+    styles,
+    sctipt,
+    optimiseImage,
+    createWebp,
+    svg,
+    sprite,
+    copy
+  )
+)
+
+gulp.task('build', build)
 
 export default gulp.series(
   styles, server, watcher
